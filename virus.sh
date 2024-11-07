20f3ad05ade4dc3fdb028ad90576da666eef28b6
Dini <DDeSilva@smail.pembinatrails.ca>
2024-11-07 15:19:38 -0600

    added virus code!

---
+++ virus.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "please list the IP adress of the user you want to infect (IP in format __.__.__.)"
+read user
+echo "now, what would you like to infect $user with? copy and paste the url of the virus!"
+read ping
+echo "downloading $ping"
+sleep 2
+echo "sending $ping"
+sleep 3
+echo "congrats! youve succsesfuly infected $user with $ping !"
+echo "goodbye, +rizz"
