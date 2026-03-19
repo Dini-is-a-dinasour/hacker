e8519b1b1b50980fe56b7999934426e05ab5b3ca
Dini-is-a-dinasour <DDeSilva@smail.pembinatrails.ca>
2026-03-19 11:52:07 -0500

    Add initial scam.sh script with user prompts
    Yo 3/19/2026, the sub and a teach are screaming at us, can't focus bruh ;(

---
+++ scam.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sleep 1
+echo "connecting to text info"
+sleep 1
+echo "please be patient, Indians are hard to find"
+sleep 5
+echo "well come du microslop securiti servises, huw may i hailp uoy?"
+echo "you can choose to say (pick the number):"
+echo "1: you called me?"
+echo "2: my computer has stopped working twin."
+read action
+echo "Im so tired i couldn't finish"
